b20fe9d45909b7c7f5b63632921629c018cbba20
Axel Smeets <murlocbrand+git@gmail.com>
2014-09-09 00:17:08 +0200

    fixed public folder mount path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var static = require('node-static');
 var http = require('http');
-var file = new(static.Server)();
+
+var file = new(static.Server)("public");
 
 // Serve files :3
 var app = http.createServer(function (req, res) {
