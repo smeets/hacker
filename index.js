ee41be2dea0797db88017b57fddd7b9121ed01a9
Axel Smeets <murlocbrand+git@gmail.com>
2014-09-09 00:55:28 +0200

    added jailed, basic html layout

---
+++ index.js
@@ -0,0 +1,9 @@
+var static = require('node-static');
+var http = require('http');
+
+var file = new(static.Server)("public");
+
+// Serve files :3
+var app = http.createServer(function (req, res) {
+	file.serve(req, res);
+}).listen(8080);
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+/* Prepare for jailed plugins */
